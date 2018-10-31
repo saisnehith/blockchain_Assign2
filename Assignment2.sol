0349b768fc46b81f7854d0c62c3eaad550f58fa5
saisnehith <g.saisnehith8978@gmail.com>
2018-10-31 21:39:53 +0530

    Updates Assignment2.sol by saisnehith

--- Assignment2.sol
+++ Assignment2.sol
@@ -9,11 +9,15 @@ contract Quiz{
     bytes q2;
     bytes q3;
     bytes q4;
+    bytes a1;
+    bytes a2;
+    bytes a3;
+    bytes a4;
     mapping(address => bool) userRegistered;
     mapping(uint => address ) userWon; // The address of the user who first answered the question will be stored here.
     address[] public userAddresses;
     
-    constructor(uint _fee, uint _duration,bytes _a,bytes _b,bytes _c,bytes _d){
+    constructor(uint _fee, uint _duration,bytes _a,bytes _e,bytes _b,bytes _f,bytes _c,bytes _g,bytes _d,bytes _h){
     timelimit = now + _duration * 1 minutes;
     pfee = _fee;
     QuizOwner=msg.sender;
@@ -21,6 +25,10 @@ contract Quiz{
     q2=_b;
     q3=_c;
     q4=_d;
+    a1=_e;
+    a2=_f;
+    a3=_g;
+    a4=_h;
     }
     
     function register() public payable {
