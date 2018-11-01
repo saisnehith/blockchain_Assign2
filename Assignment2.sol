59d1a76ccac214b1eda1a88b47a9ee8c9ac7ea7b
Aditya Agarwal <30871963+aditya3498@users.noreply.github.com>
2018-11-01 20:04:53 +0530

    Corrected payments(code is almost done)
    Permissions are left to be sorted out

--- Assignment2.sol
+++ Assignment2.sol
@@ -4,6 +4,7 @@ contract Quiz{
     uint timelimit;
     uint numberOfUsers = 0;
     uint pfee;
+    uint index = 0;
     uint tfee = 0;
     string q1;
     string q2;
@@ -14,9 +15,13 @@ contract Quiz{
     string a3;
     string a4;
     mapping(address => bool) userRegistered;
+    mapping(address => string) answeredcorrectlyq1;
+    mapping(address => string) answeredcorrectlyq2;
+    mapping(address => string) answeredcorrectlyq3;
+    mapping(address => string) answeredcorrectlyq4;
     mapping(uint => address ) userWon; // The address of the user who first answered the question will be stored here.
     mapping(address => uint) pendingReturns;
-    mapping(address => uint) answercount;
+    mapping(uint => uint) usercount;
     address[] public userAddresses;
     
     constructor(uint _fee, uint _duration,string _a,string _e,string _b,string _f,string _c,string _g,string _d,string _h){
@@ -31,6 +36,12 @@ contract Quiz{
     a2=_f;
     a3=_g;
     a4=_h;
+    }
+    
+    function test1() returns (uint)
+    {
+        address _userAddress = msg.sender;
+        return pendingReturns[_userAddress];
     }
     
     function register() public payable
@@ -71,78 +82,128 @@ contract Quiz{
    }
     
     function submitAnswer(string a, string b, string c, string d){
-        answercount[_userAddress] = 0;
         address _userAddress = msg.sender;
+        //answercount[_userAddress] = 0;
         require(userRegistered[_userAddress] == true, "Sorry not registered");     //Security Measure
         if(compareStrings(_toLower(a),_toLower(a1)) == true)
         {
-            //pendingReturns[_userAddress] += (uint(3) / uint(16)) * tfee;
-            
-            answercount[_userAddress] += 1;
+            //answercount[_userAddress] += 1;
+            //index[0] += 1;
+            usercount[0] += 1;
+            answeredcorrectlyq1[_userAddress] = "YES";
         }
         
-        else if(compareStrings(_toLower(b),_toLower(a2)) == true)
+        else
         {
-            //pendingReturns[_userAddress] += (uint(3) / uint(16)) * tfee;
-            
-            answercount[_userAddress] += 1;
+            answeredcorrectlyq1[_userAddress] = "NO";   
+            usercount[0] = 1;
         }
         
-        else if(compareStrings(_toLower(c),_toLower(a3)) == true)
+        if(compareStrings(_toLower(b),_toLower(a2)) == true)
         {
-            //pendingReturns[_userAddress] += (uint(3) / uint(16)) * tfee;
-            
-            answercount[_userAddress] += 1;
+            //answercount[_userAddress] += 1;
+            usercount[1] += 1;
+            //index[1] += 1;
+            answeredcorrectlyq2[_userAddress] = "YES";
         }
         
-        else if(compareStrings(_toLower(d),_toLower(a4)) == true)
+        else
         {
-            //pendingReturns[_userAddress] += (uint(3) / uint(16)) * tfee;
-            
-            answercount[_userAddress] += 1;
+            answeredcorrectlyq2[_userAddress] = "NO";
+            usercount[1] = 1;
+        }
+        
+        if(compareStrings(_toLower(c),_toLower(a3)) == true)
+        {
+            //answercount[_userAddress] += 1;
+            usercount[2] += 1;
+            //index[2] += 2;
+            answeredcorrectlyq3[_userAddress] = "YES";
+        }
+        
+        else
+        {
+            answeredcorrectlyq3[_userAddress] = "NO";
+            usercount[2] = 1;
+        }
+        
+        if(compareStrings(_toLower(d),_toLower(a4)) == true)
+        {
+            //answercount[_userAddress] += 1;
+            usercount[3] += 1;
+            //index[3] += 1;
+            answeredcorrectlyq4[_userAddress] = "YES";
+        }
+        
+        else
+        {
+            answeredcorrectlyq4[_userAddress] = "NO";
+            usercount[3] = 1;
         }
     }
     
-    function decideWinnerandPayRest() 
+    function decideWinnerandPayRest()
     {
+        uint perf = 0;
+        
+        require(msg.sender == QuizOwner);
+        
         for(uint i = 0; i < userAddresses.length; i++)   //To pay the pending returns for every participant
         {
-            if(answercount[userAddresses[i]] == 1)
+            if(compareStrings(answeredcorrectlyq1[userAddresses[i]],"YES"))
             {
-                pendingReturns[userAddresses[i]] += (uint(3) / uint(16)) * tfee;
+                pendingReturns[userAddresses[i]] += (3 * tfee) / (16 * usercount[0]);
+            
+                perf++;
             }
             
-            else if(answercount[userAddresses[i]] == 2)
+            if(compareStrings(answeredcorrectlyq2[userAddresses[i]],"YES"))
             {
-                pendingReturns[userAddresses[i]] += (uint(3) / uint(8)) * tfee;
+                pendingReturns[userAddresses[i]] += (3 * tfee) / (16 * usercount[1]);
+            
+                perf++;
             }
             
-            else if(answercount[userAddresses[i]] == 3)
+            if(compareStrings(answeredcorrectlyq3[userAddresses[i]],"YES"))
             {
-                pendingReturns[userAddresses[i]] += (uint(9) / uint(16)) * tfee;
-            }
-            
-            else if(answercount[userAddresses[i]] == 4)
-            {
-                pendingReturns[userAddresses[i]] += (uint(3) / uint(4)) * tfee;
+                pendingReturns[userAddresses[i]] += (3 * tfee) / (16 * usercount[2]);
                 
-                userWon[answercount[userAddresses[i]]] = userAddresses[i];
+                perf++;
             }
-        }
-        
-        uint max = 0;
-        
-        for(uint j = 0; j < userAddresses.length; j++)
-        {
-            max = answercount[userAddresses[j]];
             
-            for(uint k = j + 1; k < userAddresses.length; k++)
+            if(compareStrings(answeredcorrectlyq4[userAddresses[i]],"YES"))
             {
-                if(answercount[userAddresses[k]] > max)
-                {
-                    max = answercount[userAddresses[k]];
-                }
+                pendingReturns[userAddresses[i]] += (3 * tfee) / (16 * usercount[3]);
+                
+                perf++;
             }
+            
+            if(perf == 4)
+            {
+                userWon[index++] = userAddresses[i];
+            }
+            
         }
+    }
+    
+    function withdraw() public returns(bool)
+    {
+        uint amount = pendingReturns[msg.sender];
+        
+        if(amount > 0)
+        {
+            pendingReturns[msg.sender] = 0;
+            
+            msg.sender.transfer(amount);
+        }
+        
+        return true;
+    }
+    
+    function test() public returns(uint)
+    {
+        address _userAddress = msg.sender;
+        
+        return pendingReturns[_userAddress];
     }
 }
